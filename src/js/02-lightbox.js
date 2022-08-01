1a30f6c9a1605861dceb4b33101498e3a4bf757f
Roman_Onishchuk <romanonisuk@gmail.com>
2022-08-01 23:40:39 +0300

    task-2 done

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('beforeend', createGalleryItems(galleryItems));
 
-console.log(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a');
+lightbox.options.captionsData = 'alt';
+lightbox.options.captionDelay = 250;
+
+function createGalleryItems(items) {
+    return items.map((item) => {
+        const { preview, original, description } = item;
+
+        const itemEl = `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+        `
+        return itemEl;
+    }).join('');
+}
+
+
