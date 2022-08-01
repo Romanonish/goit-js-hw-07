d649210c3da1fd009fb998ad5b156cac717ee1c4
Roman_Onishchuk <romanonisuk@gmail.com>
2022-08-01 22:41:48 +0300

    task-1 done

---
+++ src/js/01-gallery.js
@@ -0,0 +1,55 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryEl = document.querySelector('.gallery');
+let instance;
+
+galleryEl.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+
+window.addEventListener('click', galleryItemOnClick);
+ 
+function galleryItemOnClick(e) {
+    e.preventDefault();
+    window.addEventListener('keydown', closeModalByEsc);
+
+    if (e.target.nodeName === 'IMG') {
+        const imgUrl = e.target.dataset.source;
+        instance = basicLightbox.create(showModalImg(imgUrl));
+        instance.show();
+    }   
+    
+};
+
+function closeModalByEsc(e) {
+    if (e.code === 'Escape') {        
+        instance.close();
+        document.removeEventListener('keydown', closeModalByEsc);
+};
+};
+
+function showModalImg(url) {
+    return `
+    <div class="modal">
+        <img src="${url}">
+    </div>
+    `
+};
+
+function createGallery(items) {
+    return items.map(item => {
+        const { preview, original, description } = item;
+
+        const itemEl = `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`
+        return itemEl;
+    }).join('');    
+};
